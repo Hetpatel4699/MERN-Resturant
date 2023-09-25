01eb82f6a3ad42a635ae8e00eaebb3f00b5db12a
Hetpatel4699 <hetpatel4699@gmail.com>
2023-09-25 18:34:17 +0530

    first commit

--- fronted/src/component/Header.js
+++ fronted/src/component/Header.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React from "react";
+import logo from "../assest/logo.png";
+import { Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
+import { BsCartFill } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div>
-        {/* desktop */}
-        <div className=''>
-            <div className=''>
-                <img src='' />
-            </div>
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4 md:gap-7">
+      {/* desktop */}
+      <div className="flex items-center h-full justify-between">
+        <Link to={""}>
+          <div className="h-10">
+            <img src={logo} className="h-full" />
+          </div>
+        </Link>
+
+        <div className="flex item-center gap-4 text-base md:text-lg">
+          <nav className="flex gap-6">
+            <Link to={""}>Home</Link>
+            <Link to={"menu"}>Menu</Link>
+            <Link to={"about"}>About</Link>
+            <Link to={"contact"}>Contact</Link>
+          </nav>
+          <div className="text-2x1 text-slate-600">
+            <BsCartFill />
+          </div>
+          <div className="text-2x1 text-slate-600">
+            <FaUserAlt />
+          </div>
         </div>
+      </div>
 
+      {/* mobile */}
+    </header>
+  );
+};
 
-
-        {/* mobile */}
-    </div>
-  )
-}
-
-export default Header
+export default Header;
