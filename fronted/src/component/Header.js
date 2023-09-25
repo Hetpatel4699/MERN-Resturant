6367fb24d688a1fa2590dac5c7c80dcfac72bf11
Hetpatel4699 <hetpatel4699@gmail.com>
2023-09-25 12:54:28 +0530

    first commit

---
+++ fronted/src/component/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        {/* desktop */}
+        <div className=''>
+            <div className=''>
+                <img src='' />
+            </div>
+        </div>
+
+
+
+        {/* mobile */}
+    </div>
+  )
+}
+
+export default Header
